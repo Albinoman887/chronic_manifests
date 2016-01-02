99a3213e1fb035ad04a718a5c61eeb37ce4cb000
Matt Filetto <matt.filetto@gmail.com>
2016-01-01 21:34:18 -0800

    again...
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- build-scripts/nightly-cronjob.sh
+++ build-scripts/nightly-cronjob.sh
@@ -2,7 +2,7 @@
 
 #Config options
 export PACKAGE_NAME=cm-13
-export SRC_ROOT=/home/albinoman887/android/$PACKAGE_NAME
+export SRC_ROOT=/home/albinoman887/android/cm-13
 export TMP_UPLOAD=/home/albinoman887/upload
 export REMOTE_URL=albinoman887@23.94.12.54
 export HOME_URL=albinoman887@filetto-server.duckdns.org
@@ -10,9 +10,7 @@ export WEB_ROOT=/var/www/html
 
 #######################################################################################
 
-export DEVICE="$1"
-export BUILD_TYPE="$2"
-export CURDATE=`date "+%m.%d.%Y"`
+export BUILD_TYPE=nightly
 export PATH=~/bin:$PATH
 export USE_CCACHE=1
 export CCACHE_DIR="/home/albinoman887/.ccache"
@@ -21,6 +19,7 @@ export ANDROID_CCACHE_DIR="/home/albinoman887/.ccache"
 function DoBuild()
 {
 cd $SRC_ROOT
+make clobber
 . build/envsetup.sh
 lunch cm_$DEVICE-userdebug
 echo "Device set to $DEVICE and BUILD TYPE set to $BUILD_TYPE"
@@ -30,29 +29,25 @@ mka bacon
 function SetupDownloads()
 {
 cd $SRC_ROOT/out/target/product/$DEVICE/
-cp -r $PACKAGE_NAME*.zip $TMP_UPLOAD/
+cp -r cm-13*.zip $TMP_UPLOAD/
 cd $TMP_UPLOAD
 \
 #Cop to main
-scp -p $PACKAGE_NAME*.zip $REMOTE_URL:$TMP_UPLOAD/
-ssh -t $REMOTE_URL "cd $TMP_UPLOAD ; cp -r $PACKAGE_NAME*.zip $WEB_ROOT/$DEVICE/$BUILD_TYPE/ ; rm -r $PACKAGE_NAME*.zip"
+scp -p cm-13*.zip $REMOTE_URL:$TMP_UPLOAD/
+ssh -t $REMOTE_URL "cd $TMP_UPLOAD ; cp -r cm-13*.zip $WEB_ROOT/$DEVICE/$BUILD_TYPE/ ; rm -r cm-13*.zip"
 
-rm -r $PACKAGE_NAME*.zip
+rm -r cm-13*.zip
 cd $SRC_ROOT
 }
 
 #######################################################################################
 
 #Sync
+cd $SRC_ROOT
 repo forall -c 'git reset --hard'
 repo sync --force-sync
 
-#Get time
-time_start=$(date +%s.%N)
-
-
 #Build
-BUILD_TYPE=nightly
 
 DEVICE=klte
 DoBuild
@@ -74,9 +69,5 @@ SetupDownloads
 cd $SRC_ROOT
 make clobber
 
-
-#Print total build time
-time_end=$(date +%s.%N)
-echo -e "${BLDYLW}Total time elapsed: ${TCTCLR}${TXTGRN}$(echo "($time_end - $time_start) / 60"|bc ) ${TXTYLW}minutes${TXTGRN} ($(echo "$time_end - $time_start"|bc ) ${TXTYLW}seconds) ${TXTCLR}"
 
 
